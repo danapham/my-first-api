593e20d7f00c058ccf38cd5f2fda030ba9e08e8a
Dana Pham <danapham12@gmail.com>
2021-02-16 20:56:03 -0600

    took notes on ASP.NET

---
+++ MyFirstApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyFirstApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstApi.Controllers
+{
+    [ApiController] //Attributes
+    [Route("[controller]")] //localhost: 44343/weatherforecast
+    //[Route("WeatherForecast")] //localhost: 44343/weatherforecast
+    //[Route("api/WeatherForecast")]//localhost: 44343/api/weatherforecast
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
